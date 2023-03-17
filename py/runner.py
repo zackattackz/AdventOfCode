457f338f52920c47e92cbf5ced039a54612501df
Zachary Hanham <z.hanham00@gmail.com>
2023-03-16 22:23:27 -0400

    Invalid Day/Part exit message

--- py/runner.py
+++ py/runner.py
@@ -2,6 +2,7 @@ import sys
 from importlib import import_module
 
 HELP_TEXT = "Usage: runner [day_number] [part_number]"
+INVALID_PART_TEXT = "Invalid day and/or part given: {} {}"
 
 def exit_with_msg_and_code(msg, code):
     print(msg, file=sys.stderr)
@@ -9,6 +10,9 @@ def exit_with_msg_and_code(msg, code):
 
 def exit_help():
     exit_with_msg_and_code(HELP_TEXT, 1)
+
+def exit_invalid_part(day, part):
+    exit_with_msg_and_code(INVALID_PART_TEXT.format(day, part), 2)
 
 def try_int(s):
     try:
@@ -22,7 +26,10 @@ if __name__ == "__main__":
     [day_number, part_number] = [try_int(s) for s in sys.argv[1:]]
     input_path = f"../inputs/{day_number}.txt"
     module_name = f"_{day_number}._{part_number}"
-    module = import_module(module_name)
+    try:
+        module = import_module(module_name)
+    except ModuleNotFoundError:
+        exit_invalid_part(day_number, part_number)
     with open(input_path) as f:
         print(module.answer(f))
     exit(0)
